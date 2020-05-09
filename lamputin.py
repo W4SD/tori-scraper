3577feaeb12e3a08d76ba54997f928aaa48cda96
Antti Oksman <antti.oksman101@gmail.com>
2020-05-09 12:53:02 +0300

    cleanup and basic start bot

--- lamputin.py
+++ lamputin.py
@@ -1,18 +1,24 @@
 from telegram.ext import Updater, CommandHandler
-
 import logging
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
-token = '1174679995:AAGe7e33kUt69lYmJ__XPO9qxkU34Fcf9Mw'
-updater = Updater(token=token, use_context=True)
+# API TOKEN
+with open('TOKEN') as f: TOKEN = f.readline()
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
-dispatcher = updater.dispatcher
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="I'm a bot, please talk to me!")
 
 start_handler = CommandHandler('start', start)
+
+
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
 dispatcher.add_handler(start_handler)
 
 updater.start_polling()
+
+
